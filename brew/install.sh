53b5aff1f0eb2d3cf8020a4c4c1ee42c64dec135
Masato Ishikawa <masato.ishikawa.1990@gmail.com>
2025-08-19 22:31:05 +0900

    Format package list in install script for consistency

--- brew/install.sh
+++ brew/install.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 packages=(
-  fish
-  font-hack-nerd-font
-  ghq
-  fzf
-  watch
-  htop
-  gh
-  ripgrep
-  yq
-  jq
-  podman
-  microk8s
-  multipass
-  k9s
-  kubectx
-  raycast
-  helm
-  fluxcd/tap/flux
-  controlplaneio-fluxcd/tap/flux-operator-mcp
-  kustomize
-  pyenv
-  pipenv
-  tfenv
-  direnv
-  goenv
-  tofuenv
-  kubernetes-cli
-  azure-cli
-  awscli
-  gcloud-cli
-  firebase-cli
-  mariadb-connector-c
+fish
+font-hack-nerd-font
+ghq
+fzf
+watch
+htop
+gh
+ripgrep
+yq
+jq
+podman
+microk8s
+multipass
+k9s
+kubectx
+raycast
+helm
+fluxcd/tap/flux
+controlplaneio-fluxcd/tap/flux-operator-mcp
+kustomize
+pyenv
+pipenv
+tfenv
+direnv
+goenv
+tofuenv
+kubernetes-cli
+azure-cli
+awscli
+gcloud-cli
+firebase-cli
+mariadb-connector-c
 )
 
 brew update
